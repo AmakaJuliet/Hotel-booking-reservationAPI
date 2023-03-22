c4a9507973805b1f649028803495e0cb799ba82d
Amaka Ibenegbu <113144586+AmakaJuliet@users.noreply.github.com>
2023-03-22 10:18:29 +0100

    Revert "Akjesus branch"

---
+++ controllers/room.js
@@ -0,0 +1,39 @@
+import Hotel from "../models/Hotel.js"
+import Room from "../models/Room.js"
+
+import { createError } from '../utils/error.js'
+
+export const createHotel = async (req,res, next) => {
+    const hotelId = req.params.hotelid
+    const newRoom = new Room(req.body);
+
+    try {
+        const savedRoom = await newRoom.save();
+        try {
+            await Hotel.findByIdAndUpdate(hotelId, {
+                $push: { rooms: savedRoom._id }
+            })
+        } catch (error) {
+            next(error)
+        }
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const updateRoomAvailability = async (req, res, next) => {
+    try {
+        await Room.updateOne(
+            { "roomNumbers._id": req.params.id },
+            {
+                $push : {
+                    "roomNumber.$.unavailableDates": req.body.dates
+                },
+            },
+        )
+
+        res.status(200).json("Room Status has been Updated")
+    } catch (error) {
+        next(error)
+    }
+}
--- routes/roomsRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-import { createHotel, updateRoomAvailability } from "../controllers/room.js";
-
-router.post("/", createHotel);
-router.put("/", updateRoomAvailability);
-
-export default router;
